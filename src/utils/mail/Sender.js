81e61275a06d9ff915fd21f6ca13d8ba8540c021
mistmurk <lukev21@gmail.com>
2019-06-01 15:59:00 -0400

    stashed changes

--- src/utils/mail/Sender.js
+++ src/utils/mail/Sender.js
@@ -1,3 +1,15 @@
 /*
  * A collection of utility methods used to send mail to another user
  */
+ import * as blockstack from blockstack;
+
+ module.exports = {
+   method: function sendMessageTo(message, receiverID) {
+
+     // get the users private key
+     blockstack.getFile('pk.txt', {username: receiverID, encrypt:false})
+      .then(privateKey => {
+        // first, write the message to our local storage
+      })
+   },
+ }
