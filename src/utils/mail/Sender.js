ee9d1129a39c520c777ed9704d64b8d01322eb4b
mistmurk <lukev21@gmail.com>
2019-06-01 17:14:29 -0400

    Single recepient sender

--- src/utils/mail/Sender.js
+++ src/utils/mail/Sender.js
@@ -4,12 +4,64 @@
  import * as blockstack from blockstack;
 
  module.exports = {
+   // Sends a message to a single recepient
    method: function sendMessageTo(message, receiverID) {
 
      // get the users private key
      blockstack.getFile('pk.txt', {username: receiverID, encrypt:false})
       .then(privateKey => {
-        // first, write the message to our local storage
+        const msgID = msg.id;
+        const msgString = JSON.stringify(message);
+
+        // write the message to our local storage
+        blockstack.putFile('outbox/' + msgID + '.json', msgString, {encrypt:true})
+          .then(res => {
+            console.log(res);
+          })
+          .catch(err => {
+            console.error(err);
+          })
+
+        // write the message to the receiver's outbox
+        const path = receiverID + '/outbox/' + msgID + '.json';
+        blockstack.putFile(path , msgString, {encrypt: privateKey})
+          .then(res => {
+            console.log(res);
+          })
+          .catch(err => {
+            console.error(err);
+          })
+
+        // update our global message sent list
+        blockstack.getFile('outbox/messages.json', {encrypt:true})
+          .then(messages => {
+            messagesArray = JSON.parse(messages);
+            messagesArray.push(msgID);
+            return blockstack.putFile('outbox/messages.json', JSON.stringify(messagesArray), {encrypt:true})
+          })
+          .then(res => {
+            console.log(res);
+          })
+          .catch(err => {
+            console.error(err);
+          })
+
+          // update the receiver's message sent list
+          blockstack.getFile(receiverID + '/outbox/messages.json', {encrypt:true})
+            .then(messages => {
+              messagesArray = JSON.parse(messages);
+              messagesArray.push(msgID);
+              return blockstack.putFile(receiverID + 'outbox/messages.json', JSON.stringify(messagesArray), {encrypt:true})
+            })
+            .then(res => {
+              console.log(res);
+            })
+            .catch(err => {
+              console.error(err);
+            })
       })
+      .catch(err => {
+        console.error(err);
+      });
    },
  }
